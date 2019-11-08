028adf24ac038a4e0cb0b0833d0904972fabf35e
tindoann <tindoann@gamil.com>
2019-11-07 21:30:16 -0500

    Updates

---
+++ precache-manifest.a508ffeec01365369ff509880774e3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc885ef0e6da9eee21a8f2001735abf",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "974c53cbbe55722f5a05",
+    "url": "/clicky-game/static/css/main.822299b4.chunk.css"
+  },
+  {
+    "revision": "9501f22ec6282b72990a",
+    "url": "/clicky-game/static/js/2.99673906.chunk.js"
+  },
+  {
+    "revision": "974c53cbbe55722f5a05",
+    "url": "/clicky-game/static/js/main.024ad571.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
